8523d98beebe32bd96bbf7114643c61734c863d4
Kevin Farrugia <Kevin.Farrugia@betssongroup.com>
2015-05-13 13:36:56 +0200

    Fixed async issues with tasks

--- tasks/install-dependencies.js
+++ tasks/install-dependencies.js
@@ -1,3 +1,8 @@
+'use strict';
+
+// get the command prompt
+var exec = require('child_process').exec;
+
 /*
  * grunt-contrib-install-dependencies
  * http://gruntjs.com/
@@ -5,32 +10,41 @@
  * Copyright (c) 2015 Stephen Lautier, contributors
  */
 module.exports = function(grunt) {
-    'use strict';
 
-    // get the command prompt
-    var process = require('child_process');
+    // install bower
+    function installBower(options, callback) {
+        exec('bower install', {
+            cwd: options.cwd
+        }, function(err, stdout, stderr) {
+            console.log('bower install');
+            if (!err) {
+                grunt.log.error(err);
+            }
+            console.log(stdout);
+
+            if (typeof options.callback === 'function') {
+                options.callback.call(this, err, stdout, stderr, cb);
+            } else{
+                grunt.warn('no callback function specified');
+            }
+        });
+    };
 
     grunt.registerTask('install-dependencies', 'Installs project dependencies.', function() {
 
-        // options
+        // default options
         var options = this.options({
-            cwd: './' // default to current directory
+            cwd: '.\\' // default to current directory
         });
 
-        grunt.log.writeln("Installing packages...");
-        // run dependency installers synchronously
-        grunt.task.run('install-dependencies-bower');
-    });
-
-    // install bower dependencies
-    grunt.registerTask('install-dependencies-bower', 'Installs bower dependencies', function() {
+        // set up async
         var done = this.async();
-        process.exec('bower install', {
-            cwd: options.cwd
-        }, function(err, stdout, stderr) {
-            console.log(stdout);
+
+        // run dependency installers synchronously
+        grunt.log.writeln("Installing packages...");
+        installBower(options, function() {
+            // inform grunt the task has completed
             done();
         });
     });
-
 };
